12f8d781b61afcc114fb6202eefd0d8e6d39c12d
At2345 <108577601+At2345@users.noreply.github.com>
2023-02-10 13:34:58 +0100

    Update translator.py

--- Python Project for AI & Application Development/translator.py
+++ Python Project for AI & Application Development/translator.py
@@ -13,9 +13,8 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def englishtofrench(word):
     """This class does english to french translation"""
 
-    url_lt='https://api.us-south.language-translator.watson.cloud.ibm.com/instances/dfb4e059-a2b5-470b-9ac4-f4628099cf0a'
-    apikey_lt='5W4TiN2vz1utmA3YgN0-m9qv7TgzznYioDwTXPc4g_-7'
-    version_lt='2018-05-01'
+    url_lt='https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/98eba734-0a37-4a92-ba67-9ca4a7766005'
+    version_lt='2023-02-10'
     authenticator = IAMAuthenticator(apikey_lt)
     language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
     language_translator.set_service_url(url_lt)
@@ -29,15 +28,14 @@ def englishtofrench(word):
 
     return translation['translations'][0]['translation']
 
-def englishtogerman(word):
-    """This class does english to german translation"""
+def englishtofrench(word):
+    """This class does english to french translation"""
 
-    url_lt='https://api.us-south.language-translator.watson.cloud.ibm.com/instances/dfb4e059-a2b5-470b-9ac4-f4628099cf0a'
-    apikey_lt='5W4TiN2vz1utmA3YgN0-m9qv7TgzznYioDwTXPc4g_-7'
-    version_lt='2018-05-01'
+    url_lt='https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/98eba734-0a37-4a92-ba67-9ca4a7766005'
+    version_lt='2023-02-10'
     authenticator = IAMAuthenticator(apikey_lt)
     language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
     language_translator.set_service_url(url_lt)
     lt = language_translator
-    translation = lt.translate(text=word, model_id="en-de").get_result()
+    translation = lt.translate(text=word, model_id="en-fr").get_result()
     return translation['translations'][0]['translation']
